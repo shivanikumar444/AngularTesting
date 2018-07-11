1c38fa6ffd5464b21f2b1dd2366937f2d4fa025f
shivanikumar444 <shivanikumar444@gmail.com>
2018-07-11 14:07:25 +0530

    Updated signup testcase

--- src/app/signup/signup.component.spec.ts
+++ src/app/signup/signup.component.spec.ts
@@ -14,6 +14,7 @@ import { GamelistComponent } from '../gamelist/gamelist.component';
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -43,34 +44,6 @@ describe('SignupComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should signup', () => {
-    spyOn(component, 'onSignUp');
-    const nativeElement = fixture.nativeElement;
-    const closeButton = nativeElement.querySelector('.signup');
-    closeButton.dispatchEvent(new Event('click'));
-    fixture.detectChanges();
-    expect(component.onSignUp).toHaveBeenCalledWith(true);
-  });
-});
-
-describe('SignupComponent', () => {
-  let component: SignupComponent;
-  let fixture: ComponentFixture<SignupComponent>;
-  let router: Router;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        SignupComponent,
-        HomeComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(
     async(inject([Router], (route) => {
       fixture = TestBed.createComponent(SignupComponent);
@@ -86,4 +59,47 @@ describe('SignupComponent', () => {
     expect(router.navigate).toHaveBeenCalledWith(['home']);
   });
 
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should signup', () => {
+    spyOn(component, 'onSignUp');
+    const nativeElement = fixture.nativeElement;
+    const closeButton = nativeElement.querySelector('.signup');
+    closeButton.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+    expect(component.onSignUp).toHaveBeenCalledWith(true);
+  });
 });
+
+// describe('SignupComponent', () => {
+//   let component: SignupComponent;
+//   let fixture: ComponentFixture<SignupComponent>;
+//   let router: Router;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [
+//         SignupComponent,
+//         HomeComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(
+//     async(inject([Router], (route) => {
+//       fixture = TestBed.createComponent(SignupComponent);
+//       component = fixture.debugElement.componentInstance;
+//       fixture.detectChanges();
+//       router = route;
+//     }))
+//   );
+
+//   it('should redirect to home', () => {
+//     spyOn(router, 'navigate');
+//     router.navigate(['home']);
+//     expect(router.navigate).toHaveBeenCalledWith(['home']);
+//   });
+
+// });
